abf69a9af59256d1cc5d9bcb3dd5331f59d78a34
Omertis <36013948+Omertis@users.noreply.github.com>
2018-08-29 03:05:31 +0300

    Update and rename config.js to config.json

--- config.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NDcxMjM4OTkzNjY1NTg5MjUw.Dj3iZA.DYq_pqHqV10L0ZjHtKMhXKMN9Ek'; // ya wr3 la tns5
-
-exports.PREFIX = '2';
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
